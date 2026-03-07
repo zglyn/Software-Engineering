081f9f75062a4bfb1412e5f3c586f1196557f9a6
ibrahim29asad <ibrahim.asad2019@gmail.com>
2026-03-07 13:20:57 -0500

    RoughProfile

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -1,60 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './HomePage.css';
-import { useState } from 'react';
 
 const HomePage: React.FC = () => {
-  const [uservalue, setUserValue] = useState('');
-  const [passvalue, setPassValue] = useState('');
-  const [Signup, setSignupClicked] = useState(false);
-  const [Login, setLoginClicked] = useState(false);
-
-
-  const handleSignUpClick = () => {
-    setSignupClicked(true);
-  };
-
-  const handleLoginClick = () => {
-    setLoginClicked(true);
-  };
-
   return (
-    <div >
-    {/* // <div style={{ backgroundColor: 'white', minHeight: '100vh' }}> */}
-        <div className='AppCSS'>
-          <h1>Sign Up/Login</h1>
-        </div>
-        
-        
-         <input
-          className='roundedBox'
-          type="text"
-          placeholder="Username" 
-          value={uservalue}
-          onChange={(e) => setUserValue(e.target.value)}
-         />
-
-         <input
-          className='roundedBox'
-          type="text"
-          placeholder="Password" 
-          value={passvalue}
-          onChange={(e) => setPassValue(e.target.value)}
-         />
-
-        <div className='ButtonAlign'>
-          <button onClick={handleSignUpClick} className='roundedButtons'>
-          Sign Up
-          </button>
-          <button onClick={handleLoginClick} className='roundedButtons'>
-          Login
-          </button>
-        </div>
-        
-        
-
-
-        
+    <div>
+        <Link to="/feed">Feed</Link>
     </div>
   );
 };
